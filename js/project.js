2c5cd92a32683bb6e0ab5b91e330eccc6fa552ca
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2024-04-15 12:19:19 +0200

    Fixed text sizing, button width, slider sizing and animations, contact form sizing, projects image expended background, h1 and h2 prob in html

--- js/project.js
+++ js/project.js
@@ -1,15 +1,19 @@
 //fonction pour que la croix servent a fermer l'image sélectionner
 function closeImage(){
+    let background = document.querySelector(".greyedOut")
     let container = document.getElementById("expanded-image");
     container.parentElement.style.display = "none"
+    background.style.opacity = "1"
 }
 
 
 //sert a mettre en plein écrans les image qui sont cliqués sur la gallerie
 function openImage(imgs) {
+    let background = document.querySelector(".greyedOut")
     let expandImage = document.getElementById("expanded-image");
     expandImage.src = imgs.src;
     expandImage.parentElement.style.display = "flex"
+    background.style.opacity = "0.1"
     document.documentElement.scrollTo({
         top: 0,
         left: 0,
